5a952f798620a7c4b32bfb128b47e52feebc3196
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-07-21 20:55:06 -0700

    Showcase component

---
+++ frontend/components/Showcase.js
@@ -0,0 +1,8 @@
+import styles from "@/styles/Showcase.module.css";
+
+export default function Showcase() {
+  return <div className={styles.showcase}>
+      <h1>Welcome to the Party!</h1>
+      <h2>Find the dopest DJ events</h2>
+  </div>;
+}
